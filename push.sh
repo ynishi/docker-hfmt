2b3362d5179aedff5fd53d809b026768edfaf9ce
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-09-17 12:51:29 +0000

    fix push.sh

--- push.sh
+++ push.sh
@@ -2,8 +2,12 @@
 
 set -eux
 
+BRANCH=$(git branch | grep '^\*' | grep -v master | awk '{print $2}')
+test -n "$BRANCH"
+VERSION=${VERSION:-$BRANCH}
+
 docker build --cache-from builder -t ynishi/docker-hfmt:latest .
 docker build --cache-from builder -t ynishi/docker-hfmt:${VERSION} .
 
-docker push ynishi/docker-hfmt:latest .
-docker push ynishi/docker-hfmt:${VERSION} .
+docker push ynishi/docker-hfmt:latest
+docker push ynishi/docker-hfmt:${VERSION}
